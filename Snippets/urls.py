cceca69ba02d6823a93d24a511ee3efa3eb7f70b
Roman <roma419@mail.ru>
2024-02-28 15:45:49 +0300

    Add django form for Snippet

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from MainApp import views
-
 urlpatterns = [
     path('', views.index_page, name='home'),
-    path('snippets/add', views.add_snippet_page, name='snippets-add'),
+    path('snippets/add', views.add_snippet_page, name="snippets-add"),
     path('snippets/list', views.snippets_page, name='snippets-list'),
     path('snippet/<int:snippet_id>', views.snippet_detail, name='snippet-detail'),
+    # path('snippet/create', views.create_snippet, name='create-snippet')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
