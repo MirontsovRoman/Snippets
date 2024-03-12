7fad1ff315bc33e8ffa837d0f433822ea6ed091b
Roman <roma419@mail.ru>
2024-03-12 13:48:34 +0300

    Add admin site

--- MainApp/models.py
+++ MainApp/models.py
@@ -9,6 +9,9 @@ LANGS = (
 
 
 class Snippet(models.Model):
+    class Meta:
+        ordering = ["name", "lang"]
+
     name = models.CharField(max_length=100)
     lang = models.CharField(max_length=30, choices=LANGS)
     code = models.TextField(max_length=5000)
@@ -16,5 +19,9 @@ class Snippet(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
     public = models.BooleanField(default=True)  # True - public, False - private
 
+    # В Админке используется метод __str__()
+    def __str__(self):
+        return f'Snippet({self.id}, {self.name})'
+    
     def __repr__(self):
         return f'Snippet({self.id}, {self.name}, {self.lang}, {self.user})'
